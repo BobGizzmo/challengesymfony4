f14dc4121b462661b1c17f8cf0a1e67c4a75465a
BobGizzmo <msi03@laposte.net>
2019-10-13 10:47:55 +0200

    add authenticationUtils into login methode and fr translation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,15 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/connexion", name="security_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $auth)
     {
+        //https://symfony.com/doc/current/security/form_login_setup.html
+        //Récuperation de l'erreur si l'authentification a échoué
+        
+        //Pour message fr direction config/packages/translation.yaml puis remplacer en par fr
+        //https://symfony.com/doc/current/translation.html
+        $error = $auth->getLastAuthenticationError();
+
+        //Recupère le username de la dernière tentative de connexion
+        $lastUsername = $auth->getLastUsername();
+
         return $this->render('security/index.html.twig', [
+            'error' => $error,
+            'lastUsername' => $lastUsername
         ]);
     }
 
